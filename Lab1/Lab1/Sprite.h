48cf4cfb1c58704f5bcc41e1f5c41f0bfca1ff09
Baranouski Raman <baranouski.raman@gmail.com>
2023-09-25 19:33:32 +0300

    Sprite

---
+++ Lab1/Lab1/Sprite.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <Windows.h>
+
+struct CenterPoints
+{
+	float x, y;
+};
+
+class Sprite
+{
+	HDC m_device;
+	HDC m_bufferDevice;
+	HBITMAP m_hScreen;
+	HBITMAP m_oldBmp;
+	RECT m_workSpace;
+	PAINTSTRUCT m_PStruct;
+	CenterPoints m_points;
+
+	BITMAP m_bm;
+	HDC m_spriteDevice;
+	HBITMAP m_image;
+
+public:
+	Sprite(HWND hwnd, RECT workSpace);
+
+	void draw(HWND hWnd, float X, float Y);
+	CenterPoints* center(float width, float height);
+};
+
