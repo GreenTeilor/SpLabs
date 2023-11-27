36fce8d3deca3e3a361739b4f4e2b0be241432d3
Baranouski Raman <baranouski.raman@gmail.com>
2023-11-27 21:05:54 +0300

    Fix

--- Lab3/Dll/function.cpp
+++ Lab3/Dll/function.cpp
@@ -17,34 +17,30 @@ void replaceStringInMemory(Params* params)
 		SIZE_T bytesRead = 0;
 		while (ptr < systemInfo.lpMaximumApplicationAddress) //lpMaximumApplicationAddress - pointer to the highest memory address accessible to applications and DLLs
 		{
-			int sz = VirtualQueryEx(process, ptr, &memoryInfo, sizeof(MEMORY_BASIC_INFORMATION));
-			if (sz == sizeof(MEMORY_BASIC_INFORMATION)) //memoryInfo shell contain MEMORY_BASIC_INFORMATION structure with info about the specified page range
+			int size = VirtualQueryEx(process, ptr, &memoryInfo, sizeof(MEMORY_BASIC_INFORMATION));
+			if (size == sizeof(MEMORY_BASIC_INFORMATION)) //memoryInfo shell contain MEMORY_BASIC_INFORMATION structure with info about the specified page range
 			{
 				if ((memoryInfo.State == MEM_COMMIT) && memoryInfo.AllocationProtect == PAGE_READWRITE)
 				{
 					ptr = (char*)memoryInfo.BaseAddress;
-					try {
-						BYTE* buffer = (BYTE*)malloc(memoryInfo.RegionSize);
-						if (ReadProcessMemory(process, ptr, buffer, memoryInfo.RegionSize, &bytesRead)) //Here read memory(ptr - pointer to the base address in the specified process from which to read, 
-							//buffer - pointer to a buffer that receives the contents from the address space of the specified process, lpRead - number of bytes read)
+					BYTE* buffer = (BYTE*)malloc(memoryInfo.RegionSize);
+					if (ReadProcessMemory(process, ptr, buffer, memoryInfo.RegionSize, &bytesRead)) //Here read memory(ptr - pointer to the base address in the specified process from which to read, 
+					//buffer - pointer to a buffer that receives the contents from the address space of the specified process, bytesRead - number of bytes read)
+					{
+						for (size_t i = 0; i < (bytesRead - toReplaceStrLen); ++i) //Cycle trough all bytes
 						{
-							for (size_t i = 0; i < (bytesRead - toReplaceStrLen); ++i) //Cycle trough all bytes
+							if (memcmp(params->toReplace, &buffer[i], toReplaceStrLen) == 0) //If str is found
 							{
-								if (memcmp(params->toReplace, &buffer[i], toReplaceStrLen) == 0) //If str is found
+								char* replaceMemory = ptr + i; //Add to base address position, where strToReplace was found
+								for (int j = 0; (j < (strlen(params->replaceStr)) && j < (strlen(params->toReplace))); j++)
 								{
-									char* replaceMemory = ptr + i; //Add to base address position, where strToReplace was found
-									for (int j = 0; (j < (strlen(params->replaceStr)) && j < (strlen(params->toReplace))); j++)
-									{
-										replaceMemory[j] = params->replaceStr[j];
-									}
-									replaceMemory[strlen(params->replaceStr)] = 0;
+									replaceMemory[j] = params->replaceStr[j];
 								}
+								replaceMemory[strlen(params->replaceStr)] = 0;
 							}
 						}
-						free(buffer);
 					}
-					catch (std::bad_alloc& e) { printf("%s\n", e.what()); }
-
+					free(buffer);
 				}
 			}
 			ptr += memoryInfo.RegionSize;
